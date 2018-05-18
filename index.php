440ad1b0c92cc93bc32f640d9caf9ef8afa5be8a
Salim Soumaré <salim.soumare@hotmail.fr>
2018-05-18 10:02:16 +0200

    ajout css

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
         <title>Mon site</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <link href="css/style.css" rel="stylesheet"> -->
+        <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
     <header>
-        <p>Hello Friends</p>
+        <div class="container-fluid banner">
+            <h1>Hello Friends</h1>
+        </div>
     </header>
     
     </body>
