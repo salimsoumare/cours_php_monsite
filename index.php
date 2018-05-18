c33fc84b71cc4e03bf00d13c741a810c6466dbe0
Salim Soumaré <salim.soumare@hotmail.fr>
2018-05-18 09:39:33 +0200

    première version du site

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Mon site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link href="css/style.css" rel="stylesheet"> -->
+    </head>
+    <body>
+    <header>
+        <p>Hello Friends</p>
+    </header>
+    
+    </body>
+</html>
